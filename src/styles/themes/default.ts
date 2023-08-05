aea0387fd5650d3c56360d6f976e6ea9a85fb185
netoudi <netoudi@rocketmail.com>
2023-08-05 17:40:40 -0300

    feat: add default theme

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+  primary: '#DBAC2C',
+  'primary-dark': '#C47F17',
+  'primary-light': '#F1E9C9',
+
+  secondary: '#8047F8',
+  'secondary-dark': '#4B2995',
+  'secondary-light': '#EBE5F9',
+
+  'gray-100': '#FAFAFA', // background
+  'gray-200': '#F3F2F2', // base-card
+  'gray-300': '#EDEDED', // base-input
+  'gray-400': '#E6E5E5', // base-button
+  'gray-500': '#D7D5D5', // base-hover
+  'gray-600': '#8D8686', // base-label
+  'gray-700': '#574F4D', // base-text
+  'gray-800': '#403937', // base-subtitle
+  'gray-900': '#272221', // base-title
+
+  white: '#FFFFFF',
+} as const;
