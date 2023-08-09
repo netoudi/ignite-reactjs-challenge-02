93f4df98f1fe03c7ec7b96ac7abb9acb3989004c
netoudi <netoudi@rocketmail.com>
2023-08-09 16:38:48 -0300

    feat: add success page structure

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,9 +1,55 @@
+import { CurrencyDollar, MapPin, Timer } from '@phosphor-icons/react';
 import * as S from './styles';
 
 export function Success() {
   return (
-    <S.Container>
-      <S.Content>Success</S.Content>
-    </S.Container>
+    <S.Wrapper>
+      <div>
+        <S.Title>
+          <h2>Uhu! Pedido confirmado</h2>
+          <p>Agora é só aguardar que logo o café chegará até você</p>
+        </S.Title>
+        <S.Box>
+          <S.Item>
+            <div>
+              <S.Icon data-icon="map">
+                <MapPin size={16} weight="fill" />
+              </S.Icon>
+            </div>
+            <div>
+              <p>
+                Entrega em <strong>Rua João Daniel Martinelli</strong>, 102
+              </p>
+              <p>Farrapos - Porto Alegre, RS</p>
+            </div>
+          </S.Item>
+          <S.Item>
+            <div>
+              <S.Icon data-icon="time">
+                <Timer size={16} weight="fill" />
+              </S.Icon>
+            </div>
+            <div>
+              <p>Previsão de entrega</p>
+              <strong>20 min - 30 min</strong>
+            </div>
+          </S.Item>
+          <S.Item>
+            <div>
+              <S.Icon data-icon="currency">
+                <CurrencyDollar size={16} />
+              </S.Icon>
+            </div>
+            <div>
+              <p>Pagamento na entrega</p>
+              <strong>Cartão de Crédito</strong>
+            </div>
+          </S.Item>
+        </S.Box>
+      </div>
+      <div>
+        <img src="/images/delivery.png" alt="Motoboy acelerando pra entregar seu pedido!" />
+      </div>
+    </S.Wrapper>
   );
 }
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -1,5 +1,99 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div``;
+export const Wrapper = styled.div`
+  width: 100%;
+  max-width: 115.2rem;
+  padding: 3.2rem 1.6rem;
+  margin: 0 auto;
 
-export const Content = styled.div``;
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+`;
+
+export const Title = styled.div`
+  ${({ theme }) => css`
+    margin-bottom: 4rem;
+
+    h2 {
+      color: ${theme.colors['primary-dark']};
+      font-family: 'Baloo 2';
+      font-size: 3.2rem;
+      font-style: normal;
+      font-weight: 800;
+      line-height: 130%;
+    }
+
+    p {
+      color: ${theme.colors['base-subtitle']};
+      font-family: Roboto;
+      font-size: 2rem;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 130%;
+    }
+  `};
+`;
+
+export const Box = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 3.2rem;
+
+    padding: 4rem;
+    border: 1px solid ${theme.colors['primary-dark']};
+    border-radius: 0.6rem 3.6rem;
+  `};
+`;
+
+export const Item = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+    display: flex;
+    align-items: flex-start;
+    gap: 1.2rem;
+
+    color: ${theme.colors['base-text']};
+
+    font-family: Roboto;
+    font-size: 1.6rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 130%;
+
+    strong {
+      color: ${theme.colors['base-text']};
+      font-family: Roboto;
+      font-size: 1.6rem;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 130%;
+    }
+  `};
+`;
+
+export const Icon = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    color: ${theme.colors.background};
+    padding: 0.8rem;
+    border-radius: 100%;
+
+    &[data-icon='map'] {
+      background-color: ${theme.colors.secondary};
+    }
+
+    &[data-icon='time'] {
+      background-color: ${theme.colors.primary};
+    }
+
+    &[data-icon='currency'] {
+      background-color: ${theme.colors['primary-dark']};
+    }
+  `};
+`;
