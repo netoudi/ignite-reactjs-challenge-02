915a939f56d940ab2d4e0e8b98bbc5dd8632db77
netoudi <netoudi@rocketmail.com>
2023-08-09 14:40:00 -0300

    feat: add checkout page structure

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,9 +1,142 @@
+import { Bank, CreditCard, CurrencyDollar, MapPinLine, Minus, Money, Plus, Trash } from '@phosphor-icons/react';
+import { Box } from '@app/components/Box';
+import { Divider } from '@app/components/Divider';
 import * as S from './styles';
 
 export function Checkout() {
   return (
-    <S.Container>
-      <S.Content>Checkout</S.Content>
-    </S.Container>
+    <S.Wrapper>
+      <S.ColumnLeft>
+        <h2>Complete seu pedido</h2>
+        <Box>
+          <S.Header>
+            <S.HeaderIcon>
+              <MapPinLine data-icon="map" size={22} />
+            </S.HeaderIcon>
+            <S.HeaderTitle>
+              <h3>Endereço de Entrega</h3>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
+            </S.HeaderTitle>
+          </S.Header>
+          <S.Form action="">
+            <S.FormControl>
+              <input type="text" placeholder="CEP" />
+            </S.FormControl>
+            <S.FormControl>
+              <input type="text" placeholder="Rua" />
+            </S.FormControl>
+            <S.FormControl>
+              <input type="text" placeholder="Número" />
+              <input type="text" placeholder="Complemento" />
+            </S.FormControl>
+            <S.FormControl>
+              <input type="text" placeholder="Bairro" />
+              <input type="text" placeholder="Cidade" />
+              <input type="text" placeholder="UF" />
+            </S.FormControl>
+          </S.Form>
+        </Box>
+        <Box>
+          <S.Header>
+            <S.HeaderIcon>
+              <CurrencyDollar data-icon="currency" size={22} />
+            </S.HeaderIcon>
+            <S.HeaderTitle>
+              <h3>Pagamento</h3>
+              <p>O pagamento é feito na entrega. Escolha a forma que deseja pagar</p>
+            </S.HeaderTitle>
+          </S.Header>
+          <S.PaymentType>
+            <S.Select type="button">
+              <CreditCard size={16} /> Cartão de crédito
+            </S.Select>
+            <S.Select type="button">
+              <Bank size={16} /> Cartão de débito
+            </S.Select>
+            <S.Select type="button">
+              <Money size={16} /> Dinheiro
+            </S.Select>
+          </S.PaymentType>
+        </Box>
+      </S.ColumnLeft>
+      <S.ColumnRight>
+        <h2>Cafés selecionados</h2>
+        <Box>
+          <S.Cart>
+            <S.CartItem>
+              <div>
+                <img src="/images/expresso-tradicional.png" alt="" />
+              </div>
+              <div>
+                <span>Expresso Tradicional</span>
+                <S.Actions>
+                  <S.Counter>
+                    <button>
+                      <Minus size={14} />
+                    </button>
+                    <span>0</span>
+                    <button>
+                      <Plus size={14} />
+                    </button>
+                  </S.Counter>
+                  <S.Remove>
+                    <Trash size={16} /> Remover
+                  </S.Remove>
+                </S.Actions>
+              </div>
+              <div>
+                <strong>R$ 9,90</strong>
+              </div>
+            </S.CartItem>
+            <Divider />
+            <S.CartItem>
+              <div>
+                <img src="/images/expresso-tradicional.png" alt="" />
+              </div>
+              <div>
+                <span>Expresso Tradicional</span>
+                <S.Actions>
+                  <S.Counter>
+                    <button>
+                      <Minus size={14} />
+                    </button>
+                    <span>0</span>
+                    <button>
+                      <Plus size={14} />
+                    </button>
+                  </S.Counter>
+                  <S.Remove>
+                    <Trash size={16} /> Remover
+                  </S.Remove>
+                </S.Actions>
+              </div>
+              <div>
+                <strong>R$ 9,90</strong>
+              </div>
+            </S.CartItem>
+            <Divider />
+          </S.Cart>
+          <S.Summary>
+            <div>
+              <div>Total de itens</div>
+              <div>R$ 29,70</div>
+            </div>
+            <div>
+              <div>Entrega</div>
+              <div>R$ 3,50</div>
+            </div>
+            <div>
+              <div>
+                <strong>Total</strong>
+              </div>
+              <div>
+                <strong>R$ 33,20</strong>
+              </div>
+            </div>
+          </S.Summary>
+          <S.PaymentButton type="submit">Confirmar pedido</S.PaymentButton>
+        </Box>
+      </S.ColumnRight>
+    </S.Wrapper>
   );
 }
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,5 +1,314 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div``;
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+    max-width: 115.2rem;
+    padding: 3.2rem 1.6rem;
+    margin: 0 auto;
 
-export const Content = styled.div``;
+    display: flex;
+    gap: 3.2rem;
+
+    h2 {
+      color: ${theme.colors['base-subtitle']};
+      font-family: 'Baloo 2';
+      font-size: 1%.;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 130%;
+    }
+
+    h3 {
+      color: ${theme.colors['base-subtitle']};
+      font-family: Roboto;
+      font-size: 1.6rem;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 130%;
+    }
+
+    p {
+      color: ${theme.colors['base-text']};
+      font-family: Roboto;
+      font-size: 1.4rem;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 130%;
+    }
+  `};
+`;
+
+export const ColumnLeft = styled.div`
+  width: 100%;
+  max-width: 64rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+`;
+
+export const ColumnRight = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: 0.8rem;
+`;
+
+export const HeaderIcon = styled.div`
+  ${({ theme }) => css`
+    svg[data-icon='map'] {
+      color: ${theme.colors['primary-dark']};
+    }
+
+    svg[data-icon='currency'] {
+      color: ${theme.colors['secondary']};
+    }
+  `};
+`;
+
+export const HeaderTitle = styled.div``;
+
+export const Form = styled.form`
+  ${({ theme }) => css`
+    margin-top: 3.2rem;
+
+    display: flex;
+    flex-direction: column;
+    gap: 1.6rem;
+
+    input {
+      display: flex;
+      width: 100%;
+      padding: 1.2rem;
+      align-items: center;
+      gap: 0.4rem;
+
+      border-radius: 0.4rem;
+      border: 1px solid ${theme.colors['base-button']};
+      background: ${theme.colors['base-input']};
+    }
+  `};
+`;
+
+export const FormControl = styled.div`
+  display: flex;
+  gap: 1.6rem;
+`;
+
+export const PaymentType = styled.div`
+  display: flex;
+  gap: 1.2rem;
+  margin-top: 3.2rem;
+`;
+
+export const Select = styled.button`
+  ${({ theme }) => css`
+    border: none;
+    cursor: pointer;
+
+    display: flex;
+    padding: 1.6rem;
+    align-items: center;
+    gap: 1.2rem;
+    flex: 1 0 0;
+
+    border-radius: 0.6rem;
+    background: ${theme.colors['base-button']};
+
+    color: ${theme.colors['base-text']};
+    font-family: Roboto;
+    font-size: 1.2rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 160%;
+    text-transform: uppercase;
+
+    svg {
+      color: ${theme.colors['secondary']};
+    }
+  `};
+`;
+
+export const PaymentButton = styled.button`
+  ${({ theme }) => css`
+    border: none;
+    cursor: pointer;
+
+    width: 100%;
+
+    display: flex;
+    padding: 1.2rem 0.8rem;
+    justify-content: center;
+    align-items: center;
+    gap: 0.4rem;
+    align-self: stretch;
+
+    border-radius: 0.6rem;
+    background: ${theme.colors.primary};
+
+    color: ${theme.colors.white};
+    font-family: Roboto;
+    font-size: 1.4rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 160%;
+    text-transform: uppercase;
+  `};
+`;
+
+///
+/// Cart
+///
+export const Cart = styled.div``;
+
+export const CartItem = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    gap: 1.6rem;
+
+    img {
+      width: 6.4rem;
+      height: auto;
+    }
+
+    & > div:last-child {
+      margin-left: auto;
+    }
+
+    color: ${theme.colors['base-subtitle']};
+    font-family: Roboto;
+    font-size: 1.6rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 130%;
+
+    strong {
+      color: ${theme.colors['base-text']};
+      font-family: Roboto;
+      font-size: 1.6rem;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 130%;
+    }
+  `};
+`;
+
+export const Actions = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.8rem;
+
+  margin-top: 0.8rem;
+`;
+
+export const Counter = styled.div`
+  ${({ theme }) => css`
+    height: 3.2rem;
+    padding: 0 0.8rem;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0.4rem;
+
+    border-radius: 0.6rem;
+    background: ${theme.colors['base-button']};
+
+    color: ${theme.colors['base-title']};
+
+    font-family: Roboto;
+    font-size: 1.6rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 130%;
+
+    button {
+      cursor: pointer;
+      width: 1.4rem;
+      height: 1.4rem;
+      border: none;
+      background: transparent;
+      line-height: 0;
+    }
+
+    span {
+      width: 3rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      line-height: 0;
+    }
+  `};
+`;
+
+export const Remove = styled.button`
+  ${({ theme }) => css`
+    border: none;
+    background: transparent;
+    cursor: pointer;
+    line-height: 0;
+
+    color: ${theme.colors['base-text']};
+
+    display: flex;
+    height: 3.2rem;
+    padding: 0 0.8rem;
+    justify-content: center;
+    align-items: center;
+    gap: 0.4rem;
+
+    border-radius: 0.6rem;
+    background: ${theme.colors['base-button']};
+
+    font-family: Roboto;
+    font-size: 1.2rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 160%;
+    text-transform: uppercase;
+
+    svg {
+      color: ${theme.colors.secondary};
+    }
+  `};
+`;
+
+export const Summary = styled.div`
+  ${({ theme }) => css`
+    margin-bottom: 2.4rem;
+
+    display: flex;
+    flex-direction: column;
+    gap: 1.2rem;
+
+    color: ${theme.colors['base-text']};
+    font-family: Roboto;
+    font-size: 1.4rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 130%;
+
+    & > div {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    strong {
+      color: ${theme.colors['base-subtitle']};
+      font-family: Roboto;
+      font-size: 2rem;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 130%;
+    }
+  `};
+`;
